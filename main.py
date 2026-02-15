a75200c81f678842ccab518a4158fe707f1d6e70
Kml251 <2024455@student.cct.ie>
2026-02-15 06:19:26 +0000

    Save the input as a file named after the input word.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os # Standard library for file/folder operations
+
 user_input = input("Please enter a word: ").upper()
+
+# Check if the directory exists; if not, create it
+if not os.path.exists("morse_code_output"):
+    os.makedirs("morse_code_output")
 
 with open("morse_code.txt", "r") as file:
     # Read the text and convert the string representation of the dict into a real dict
@@ -15,4 +21,9 @@ for character in user_input:
     if character in morse_dict:
         # Build the Morse string (adding a space between letters)
         encoded_word += morse_dict[character] + " "
-print(encoded_word)
+
+# Save the result to a file named after the input word
+# Make sure the folder 'morse_code_output' actually exists!
+with open(f"morse_code_output/{user_input}.txt", "w") as file:
+    file.write(encoded_word)
+    print(f"File saved successfully for: {user_input} = {encoded_word}")
